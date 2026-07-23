111f5c0e346a7a34dcf94def54a40048e96a7b3d
irfana-ron <irron173@gmail.com>
2026-07-23 17:54:20 +0530

    commit

--- src/main/java/com/sak/controller/HospitalController.java
+++ src/main/java/com/sak/controller/HospitalController.java
@@ -1,4 +1,4 @@
-package com.example.hospital.controller;
+package com.sak.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
